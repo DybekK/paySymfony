8e0ee4404e276b23f953e4ba875d2e165e9d081e
DybekK <matdybowski@gmail.com>
2020-02-24 16:57:39 +0100

    working jwt

--- src/Controller/Api/TestController.php
+++ src/Controller/Api/TestController.php
@@ -3,26 +3,50 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\User;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class TestController extends AbstractFOSRestController
 {
 
     /**
-     * Creates an Article resource
-     * @Rest\Get("/test")
+     * @Rest\Get("/jwt/test")
      * @return View
      */
     public function getTest(): View
     {
 
         $data = ['imie'=>'Mateusz'];
-        // In case our POST was a success we need to return a 201 HTTP CREATED response
         return View::create($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Post("/register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @return View
+     */
+    public function postRegister(Request $request, UserPasswordEncoderInterface $encoder): View
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $username = $request->get('_username');
+        $email = $request->get('_email');
+        $password = $request->get("_password");
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $em->persist($user);
+        $em->flush();
+
+        return View::create(sprintf('User %s successfully created', $user->getUsername()), Response::HTTP_OK);
     }
 }
